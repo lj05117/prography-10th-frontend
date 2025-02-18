74c59405bd12e8be3e8867a6d100d2707b269381
Jiye Lee <lj05117@naver.com>
2025-02-18 22:20:27 +0900

    feat(button) : 버튼 공통 스타일 생성

---
+++ src/styles/button.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+interface ButtonProps {
+  variant?: "primary" | "secondary"; // 배경색 설정
+  size?: "small" | "large" | "wide"; // 버튼 크기 설정
+}
+
+const Button = styled.button<ButtonProps>`
+  border: none;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  transition:
+    background-color 0.3s,
+    color 0.3s;
+
+  /* 배경색 설정 */
+  background-color: ${({ variant }) =>
+    variant === "primary" ? "#007bff" : "#ccc"};
+  color: ${({ variant }) => (variant === "primary" ? "white" : "#333")};
+
+  /* 크기 설정 */
+  height: ${({ size }) =>
+    size === "small" ? "32px" : size === "large" ? "48px" : "32px"};
+  width: ${({ size }) =>
+    size === "small" ? "100px" : size === "large" ? "200px" : "200px"};
+
+  &:hover {
+    background-color: ${({ variant }) =>
+      variant === "primary" ? "#0056b3" : "#aaa"};
+  }
+`;
+
+export default Button;
