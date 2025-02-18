f0127b9b656e7d8bfda9404b556d1ad63a1863f6
Jiye Lee <lj05117@naver.com>
2025-02-18 23:00:48 +0900

    style(layout) : 컴포넌트 테두리 스타일 생성

---
+++ src/styles/layout.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const RoundedBorderBox = styled.div`
+  margin: 16px;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+  padding: 16px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 100%;
+  max-width: 400px;
+  background-color: white;
+`;
