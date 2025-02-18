77a6b11b886657ea080a1aaf9779b9f83fa8f3f1
Jiye Lee <lj05117@naver.com>
2025-02-18 21:45:33 +0900

    feat(recruit-funnel) : 리쿠르팅 퍼널 페이지 생성

---
+++ src/pages/recruit-funnel/RecruitFunnel.tsx
@@ -0,0 +1,76 @@
+import PrivacyPolicy from "./PrivacyPolicy.tsx";
+import PersonalInfo from "./PersonalInfo.tsx";
+import ApplicationInfo from "./ApplicationInfo.tsx";
+import Complete from "./Complete.tsx";
+import ProgressStage from "../../components/ProgressStage.tsx";
+import Header from "../../components/Header.tsx";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background: white; /* 배경색 추가 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
+  z-index: 1000;
+`;
+
+const ContentContainer = styled.div`
+  padding-top: 100px; /* 헤더 + 진행 바 높이만큼 패딩 추가 */
+  text-align: center;
+`;
+
+export default function RecruitFunnel() {
+  const [recruitData, setRecruitData] = useState();
+  const [step, setStep] = useState(1); //todo: 페이지 별 값 설정
+  return (
+    <>
+      <Container>
+        <Header />
+        <>{step < 4 && <ProgressStage step={step} />}</>
+      </Container>
+      <ContentContainer>
+        <>
+          {step === 1 && (
+            <PrivacyPolicy
+              onNext={(data) => {
+                setRecruitData((prev) => ({ ...prev, privacy: data }));
+                setStep(2);
+              }}
+            />
+          )}
+        </>
+        <>
+          {step === 2 && (
+            <PersonalInfo
+              onNext={(data) => {
+                setRecruitData((prev) => ({ ...prev, personal: data }));
+                setStep(3);
+              }}
+            />
+          )}
+        </>
+        <>
+          {step === 3 && (
+            <ApplicationInfo
+              onNext={(data) => {
+                setRecruitData((prev) => ({ ...prev, application: data }));
+                setStep(4);
+              }}
+            />
+          )}
+        </>
+        <>{step === 4 && <Complete />}</>
+        {/*  //todo: 이 컴포넌트에서 tanstack query
+        연결하기*/}
+      </ContentContainer>
+    </>
+  );
+}
