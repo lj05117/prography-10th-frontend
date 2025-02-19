c99590142afe7f7d50ad10d59df3fad13556333e
Jiye Lee <lj05117@naver.com>
2025-02-19 20:33:08 +0900

    feat(input) : 입력 컴포넌트 생성

---
+++ src/components/InputInfo.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  margin: 16px;
+  width: 100%;
+  padding: 12px 16px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  outline: none;
+  transition: border 0.3s ease;
+
+  &::placeholder {
+    color: #aaa;
+    font-style: italic;
+  }
+
+  &:focus {
+    border-color: #007bff;
+  }
+`;
+
+export default function InputInfo({ placeholder, value }) {
+  return <StyledInput type="text" placeholder={placeholder} value={value} />;
+}
