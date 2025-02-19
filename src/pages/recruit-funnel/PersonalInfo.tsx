463f6fcc46e6624cb12016999aadb23dbb1edb0a
Jiye Lee <lj05117@naver.com>
2025-02-19 20:34:08 +0900

    feat(personal-info) : 기본정보 페이지 입력 컴포넌트 추가

--- src/pages/recruit-funnel/PersonalInfo.tsx
+++ src/pages/recruit-funnel/PersonalInfo.tsx
@@ -1,3 +1,14 @@
-export default function PersonalInfo() {
-  return <div>PersonalInfo</div>;
+import RecruitFunnel from "./RecruitFunnel.tsx";
+import Header from "../../components/Header.tsx";
+import ProgressStage from "../../components/ProgressStage.tsx";
+import InputInfo from "../../components/InputInfo.tsx";
+
+export default function PersonalInfo({ name, email, phone }) {
+  return (
+    <>
+      <InputInfo placeholder={"예시 : 홍길동"} value={name} />
+      <InputInfo placeholder={"예시 : prography@gmail.com"} value={email} />
+      <InputInfo placeholder={"예시 : 010-1234-5678"} value={phone} />
+    </>
+  );
 }
