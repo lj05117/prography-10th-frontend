a3412334834f174bd8a5dbd11ce856f0488f7780
Jiye Lee <lj05117@naver.com>
2025-02-19 21:09:26 +0900

    feat(content-container) : 스타일 적용을 위한 컴포넌트 생성

---
+++ src/components/ContentContainer.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 20px auto;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #f9f9f9;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const Title = styled.h2`
+  margin-bottom: 16px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #333;
+  text-align: start;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export default function ContentContainer({ title, children }) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Content>{children}</Content>
+    </Container>
+  );
+}
