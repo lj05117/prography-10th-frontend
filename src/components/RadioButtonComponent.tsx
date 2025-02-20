10a25a5d8f3b4f675fc22c4761b1ec5f856ef65d
Jiye Lee <lj05117@naver.com>
2025-02-20 12:59:08 +0900

    feat(radio-button) : 라디오 버튼 컴포넌트 수정

--- src/components/RadioButtonComponent.tsx
+++ src/components/RadioButtonComponent.tsx
@@ -1,23 +1,25 @@
-import { useRecoilState } from "recoil";
-import { selectedRadioState } from "../stores/ButtonState";
 import { RoundedBorderBox } from "../styles/layout.ts";
 
 interface RadioProps {
   id: number;
   label: string;
 }
-export default function RadioButtonComponent({ id, name, label }: RadioProps) {
-  const [selectedValue, setSelectedValue] = useRecoilState(selectedRadioState);
-
+export default function RadioButtonComponent({
+  id,
+  name,
+  label,
+  onSelect,
+  selected,
+}: RadioProps) {
   return (
     <>
-      <RoundedBorderBox onClick={() => setSelectedValue(id)}>
+      <RoundedBorderBox onClick={() => onSelect(id)}>
         <label>
           <input
             type="radio"
             name={name}
-            checked={selectedValue === id}
-            onChange={() => setSelectedValue(id)}
+            checked={selected}
+            onChange={() => onSelect(id)}
           />
           {label}
         </label>
