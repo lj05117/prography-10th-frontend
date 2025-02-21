ec2b9b3ab8fd1d425601d4a7ef7407c49a47cd17
Jiye Lee <lj05117@naver.com>
2025-02-21 13:43:29 +0900

    style(recruit-funnel) : 완료 페이지 글자 스타일 수정

--- src/pages/recruit-funnel/Complete.tsx
+++ src/pages/recruit-funnel/Complete.tsx
@@ -1,14 +1,15 @@
 import { Button } from "../../styles/button.ts";
 import { useNavigate } from "react-router-dom";
+import {StyledText} from "../../styles/string.ts";
 
 export default function Complete() {
   const navigate = useNavigate();
 
   return (
     <>
-      <div>지원이 완료되었습니다!</div>
-      <div>프로그라피 10기 지원재주셔서 감사합니다.</div>
-      <div>서류 심사 결과는 입력하신 이메일로 안내드릴 예정입니다.</div>
+      <StyledText>지원이 완료되었습니다!</StyledText>
+      <StyledText>프로그라피 10기 지원재주셔서 감사합니다.</StyledText>
+      <StyledText>서류 심사 결과는 입력하신 이메일로 안내드릴 예정입니다.</StyledText>
       <Button
         variant={"primary"}
         size="large"
